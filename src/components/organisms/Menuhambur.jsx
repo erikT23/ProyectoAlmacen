3e7c5fe38ad464a317b9534baa6fdc83df232532
ErikTapia <20203tn152@utez.edu.mx>
2024-01-12 18:03:30 -0500

    Refactor MenuHambur component and add click functionality

--- src/components/organisms/Menuhambur.jsx
+++ src/components/organisms/Menuhambur.jsx
@@ -1,19 +1,54 @@
 import styled from "styled-components";
-
+import { LinksArray, SecondarylinksArray, ToggleTema } from "../../index";
+import { NavLink } from "react-router-dom";
+import { v } from "../../styles/variables";
+import { useState } from "react";
 export function MenuHambur() {
+  const { click, setClick } = useState(false);
   return (
     <Container>
       <NavBar>
         <section>
-          <HamburgerMenu>
-            <input type="checkbox" id="checkbox" onClick={} />
-            <label htmlFor="checkbox" className="toggle">
+          <HamburgerMenu onClick={() => setClick}>
+            <label
+              htmlFor="checkbox"
+              className={click ? "toogle active" : "toogle"}
+            >
               <div className="bars" id="bar1"></div>
               <div className="bars" id="bar2"></div>
               <div className="bars" id="bar3"></div>
             </label>
           </HamburgerMenu>
         </section>
+        <Main $click={click.toString()}>
+          {LinksArray.map(({ icon, label, to }) => (
+            <div
+              onClick={() => setClick(!click)}
+              className="LinkContainer"
+              key={label}
+            >
+              <NavLink to={to} className="Links">
+                <div className="Linkicon">{icon}</div>
+                <span>{label}</span>
+              </NavLink>
+            </div>
+          ))}
+          <Divider />
+          {SecondarylinksArray.map(({ icon, label, to }) => (
+            <div
+              onClick={() => setClick(!click)}
+              className="LinkContainer"
+              key={label}
+            >
+              <NavLink to={to} className="Links">
+                <div className="Linkicon">{icon}</div>
+                <span>{label}</span>
+              </NavLink>
+            </div>
+          ))}
+          <ToggleTema />
+          <Divider />
+        </Main>
       </NavBar>
     </Container>
   );
@@ -92,4 +127,21 @@ const HamburgerMenu = styled.span`
     transition-duration: 0.5s;
     transform: rotate(180deg);
   }
+`;
+
+const Main = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  z-index: 10;
+  @media screen {
+  }
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  width: 100%;
+  background: ${(props) => props.theme.bg4};
+  margin: ${() => v.lgSpacing} 0;
 `;
