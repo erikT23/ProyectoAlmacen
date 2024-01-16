8b26e21092521007871047cadb780e163bb11a5a
ErikTapia <20203tn152@utez.edu.mx>
2024-01-16 16:02:19 -0500

    Add Btnsave component to LoginTemplate andvance halted before reunion

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
+import { Btnsave } from "../../components/index";
+import { useUsersStore } from "../../store/index";
 export function LoginTemplate() {
+  const { insertAdminUser } = useUsersStore();
   return (
     <Container>
       <h1>hola login template</h1>
+      <Btnsave titulo="Crear cuenta" bgcolor="#3AA597" />
     </Container>
   );
 }
 
 const Container = styled.div`
- 
+  height: 100vh;
 `;
