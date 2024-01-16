e7d424145f9c59e0853e5d992de642fa9dc5d456
ErikTapia <20203tn152@utez.edu.mx>
2024-01-16 17:48:24 -0500

    Update @tanstack/react-query version to 5.17.15

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 import { Btnsave } from "../../components/index";
 import { useUsersStore } from "../../store/index";
+import { useMutation } from "@tanstack/react-query";
 export function LoginTemplate() {
   const { insertAdminUser } = useUsersStore();
+  const mutation = useMutation({
+    mutationKey: ["insertar usuario Admin"],
+    mutationFn: async () => {
+      await insertAdminUser;
+    },
+  });
   return (
     <Container>
       <h1>hola login template</h1>
