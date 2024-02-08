8d8ef981d7949e3f876ade2a503d7fcb3684cdb8
ErikTapia <20203tn152@utez.edu.mx>
2024-02-08 15:05:51 -0500

    Add InsertTest function to TestStore

--- src/store/TestStore.jsx
+++ src/store/TestStore.jsx
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { ShowTest, CountTest, SearchTest } from "../supabase/index";
+import { ShowTest, CountTest, SearchTest, InsertTest } from "../supabase/index";
 
 export const useTestStore = create((set, get) => ({
   datatest: [],
@@ -22,8 +22,22 @@ export const useTestStore = create((set, get) => ({
   data: [],
   item: [],
   parametros: {},
-  mostrarMarca: async (p) => {
-    const response = await SearchTest(p)
-    
+  mostrarTest: async (p) => {
+    const response = await SearchTest(p);
+    set({ parametros: response });
+    set({ data: response });
+    set({ item: response[0] });
+    return response;
+  },
+
+  selectTest: (p) => {
+    set({ item: p });
+  },
+
+  insertTest: async (p) => {
+    await InsertTest(p);
+
+    const { mostrarTest } = get();
+    set(mostrarTest);
   },
 }));
