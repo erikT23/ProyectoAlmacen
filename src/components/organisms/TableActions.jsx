5536343d87037ea0580224b3da15bbfa5f8492b0
ErikTapia <20203tn152@utez.edu.mx>
2024-02-10 12:31:57 -0500

    Add edit and delete functionality to TableActions component

--- src/components/organisms/TableActions.jsx
+++ src/components/organisms/TableActions.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 import { AccionTabla } from "../atoms/index";
+import { v } from "../../styles/index";
 
-export function TableActions() {
+export function TableActions({ editFunct, deleteFunct }) {
   return (
     <Container>
-      <AccionTabla />
+      <AccionTabla
+        funcion={editFunct}
+        color="#aac50d"
+        icono={<v.iconeditarTabla />}
+        fontsize="20px"
+      />
+      <AccionTabla
+        funcion={deleteFunct}
+        color="#aac50d"
+        icono={<v.iconeliminarTabla />}
+        fontsize="20px"
+      />
     </Container>
   );
 }
